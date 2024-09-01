f4ad6ae713d6f8951e01f7fa8b05234d8dd61830
ashlynm248 <a_martinez26@mail.fhsu.edu>
2024-09-01 08:23:31 -0500

    Add Assignment 2

---
+++ assignment 2/main.js
@@ -0,0 +1,47 @@
+let myName= 'Will'; 
+let myAge= '33';
+console.log("Name: " +(myName)+ "");
+console.log("Age: " +(myAge)+ "");
+let student = true; 
+console.log("Will is a student: " +(student)+ "")
+
+
+const integer1 = 6;
+const integer2 = 4;
+const integer3 = integer1 + integer2; 
+const integer4 = integer1 - integer2; 
+const integer5 = integer1 * integer2;
+const integer6 = integer1 / integer2; 
+console.log(integer3);
+console.log(integer4);
+console.log(integer5);
+console.log(integer6);
+
+
+const string1 = "Winter is the best because it snows!";
+console.log(string1.length)
+console.log(string1.charAt(0, 0))
+console.log(string1.charAt(35,35))
+
+
+const negative1 = -2; 
+console.log(Math.sqrt(negative1));
+console.log(Math.pow(negative1, 2));
+console.log(Math.abs(negative1));
+
+
+const number1 = 9;
+const number2 = 21; 
+console.log("number1" < "number2")
+
+
+let purple = true;
+let green = false; 
+console.log(purple && green); 
+console.log(purple || green);
+console.log(!purple); 
+
+
+let firstname = 'Ray';
+let lastname= ' Charles'; 
+console.log("Hello " +(firstname + lastname)+ ". I hope you have a great day!");
